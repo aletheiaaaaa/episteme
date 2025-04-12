5a04350b92c56a86281d033330be8535df1f6cf5
Yash <yasaswi.rallabandi@gmail.com>
2025-04-12 17:16:45 +0530

    minor tweaks

---
+++ build/CMakeFiles/valhalla.exe.dir/DependInfo.cmake
@@ -0,0 +1,27 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/home/yash/Desktop/Valhalla/src/chess/move.cpp" "CMakeFiles/valhalla.exe.dir/src/chess/move.cpp.o" "gcc" "CMakeFiles/valhalla.exe.dir/src/chess/move.cpp.o.d"
+  "/home/yash/Desktop/Valhalla/src/chess/movegen.cpp" "CMakeFiles/valhalla.exe.dir/src/chess/movegen.cpp.o" "gcc" "CMakeFiles/valhalla.exe.dir/src/chess/movegen.cpp.o.d"
+  "/home/yash/Desktop/Valhalla/src/chess/perft.cpp" "CMakeFiles/valhalla.exe.dir/src/chess/perft.cpp.o" "gcc" "CMakeFiles/valhalla.exe.dir/src/chess/perft.cpp.o.d"
+  "/home/yash/Desktop/Valhalla/src/chess/position.cpp" "CMakeFiles/valhalla.exe.dir/src/chess/position.cpp.o" "gcc" "CMakeFiles/valhalla.exe.dir/src/chess/position.cpp.o.d"
+  "/home/yash/Desktop/Valhalla/src/main.cpp" "CMakeFiles/valhalla.exe.dir/src/main.cpp.o" "gcc" "CMakeFiles/valhalla.exe.dir/src/main.cpp.o.d"
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_LINKED_INFO_FILES
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_FORWARD_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ build/CMakeFiles/valhalla.exe.dir/cmake_clean.cmake
@@ -0,0 +1,19 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/valhalla.exe.dir/src/chess/move.cpp.o"
+  "CMakeFiles/valhalla.exe.dir/src/chess/move.cpp.o.d"
+  "CMakeFiles/valhalla.exe.dir/src/chess/movegen.cpp.o"
+  "CMakeFiles/valhalla.exe.dir/src/chess/movegen.cpp.o.d"
+  "CMakeFiles/valhalla.exe.dir/src/chess/perft.cpp.o"
+  "CMakeFiles/valhalla.exe.dir/src/chess/perft.cpp.o.d"
+  "CMakeFiles/valhalla.exe.dir/src/chess/position.cpp.o"
+  "CMakeFiles/valhalla.exe.dir/src/chess/position.cpp.o.d"
+  "CMakeFiles/valhalla.exe.dir/src/main.cpp.o"
+  "CMakeFiles/valhalla.exe.dir/src/main.cpp.o.d"
+  "valhalla.exe"
+  "valhalla.exe.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/valhalla.exe.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
