6bde2342b0c02c5843ae678ccc8d787a1c2c67c1
Yash Rallabandi <carbon674@gmail.com>
2025-05-31 10:31:15 +0530

    minimal

--- src/main.cpp
+++ src/main.cpp
@@ -13,22 +13,18 @@
 
 using namespace episteme;
 
-// int main(int argc, char *argv[]) {
-//     search::Parameters params = {};
-//     std::string line;
+int main(int argc, char *argv[]) {
+    search::Parameters params = {};
+    std::string line;
 
-//     while (std::getline(std::cin, line)) {
-//         if (line == "bench") {
-//             search::Worker thread;
-//             thread.bench();
-//         } else {
-//             parse(line, params);
-//         }
-//     }
-//     return 0;
-// }
-
-int main() {
-    search::Worker thread;
-    thread.bench();
+    while (std::getline(std::cin, line)) {
+        if (line == "bench") {
+            search::Worker thread;
+            thread.bench();
+        } else {
+            parse(line, params);
+        }
+    }
+    return 0;
 }
+
