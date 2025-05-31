96c70ed4bb16965b3c0360061aefe3cfb38e2692
Yash Rallabandi <carbon674@gmail.com>
2025-05-31 10:35:58 +0530

    add cli arg support

--- src/main.cpp
+++ src/main.cpp
@@ -15,16 +15,32 @@ using namespace episteme;
 
 int main(int argc, char *argv[]) {
     search::Parameters params = {};
-    std::string line;
 
-    while (std::getline(std::cin, line)) {
-        if (line == "bench") {
+    if (argc > 1) {
+        std::string cmd;
+        for (int i = 1; i < argc; ++i) {
+            cmd += argv[i];
+            if (i < argc - 1) cmd += ' ';
+        }
+
+        if (cmd == "bench") {
             search::Worker thread;
             thread.bench();
         } else {
-            parse(line, params);
+            parse(cmd, params);
         }
+    } else {
+        std::string line;
+        while (std::getline(std::cin, line)) {
+            if (line == "bench") {
+                search::Worker thread;
+                thread.bench();
+            } else {
+                parse(line, params);
+            }
+        }    
     }
+
     return 0;
 }
 
