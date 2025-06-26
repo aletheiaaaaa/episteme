e709cbc26a9ddcc2106d5b8d9e44048e76efc357
Yash Rallabandi <carbon674@gmail.com>
2025-06-26 20:08:44 +0530

    fix fp x4 -e bench: 565950 -e bench: 534336

--- src/search/search.cpp
+++ src/search/search.cpp
@@ -134,7 +134,7 @@ namespace episteme::search {
 
             if (is_quiet && num_legal >= 6 + 2 * depth * depth) break;
 
-            if (!is_PV && is_quiet && !in_check(position, position.STM()) && static_eval + depth * 250 < alpha) continue;
+            if (!is_PV && is_quiet && !in_check(position, position.STM()) && static_eval + depth * 250 <= alpha) continue;
 
             accumulator = eval::update(position, move, accumulator);
             accum_history.emplace_back(accumulator);
