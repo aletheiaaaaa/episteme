825706c8698d04a7a85c2c3b47b72d6acb595730
Yash Rallabandi <carbon674@gmail.com>
2025-07-17 20:12:45 +0530

    double extensions -e bench: 714796

--- src/engine/search/search.cpp
+++ src/engine/search/search.cpp
@@ -170,7 +170,10 @@ namespace episteme::search {
 
                 if (should_stop) return 0;
 
-                if (score < new_beta) extension = 1;
+                if (score < new_beta) {
+                    if (!is_PV && score < new_beta - 10) extension = 2;
+                    else extension = 1;
+                };
                 else if (new_beta >= beta && std::abs(score) < MATE - MAX_SEARCH_PLY) return new_beta;
             }
 
