8ecd12c12b78606231b6eea8e633cae907198307
Yash Rallabandi <carbon674@gmail.com>
2025-07-20 18:23:39 +0530

    oops -e bench: 2964658

--- src/engine/search/history.h
+++ src/engine/search/history.h
@@ -39,11 +39,11 @@ namespace episteme::hist {
 
                 auto get_hist = [&](int16_t diff) {
                     int16_t ply_value = 0;
-                    if (ply >= diff - 1) {
+                    if (ply >= diff) {
                         Move prev_move = stack[ply - diff].move;
                         Piece prev_piece = stack[ply - diff].piece;
 
-                        if (prev_move.data()) ply_value += cont_hist[piece_idx(piece)][sq_idx(move.to_square())][piece_idx(prev_piece)][sq_idx(prev_move.to_square())].value;
+                        if (prev_move.data() && move.data()) ply_value += cont_hist[piece_idx(piece)][sq_idx(move.to_square())][piece_idx(prev_piece)][sq_idx(prev_move.to_square())].value;
                     }
                     return ply_value;
                 };
@@ -60,7 +60,7 @@ namespace episteme::hist {
 
             inline void update_cont_hist(stack::Stack& stack, Piece piece, Move move, int16_t bonus, int16_t ply) {
                 auto update_hist = [&](int16_t diff) {
-                    if (ply >= diff - 1) {
+                    if (ply >= diff) {
                         Move prev_move = stack[ply - diff].move;
                         Piece prev_piece = stack[ply - diff].piece;
 
